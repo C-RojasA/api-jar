cd0292ae6d78109900cb7c03358d6e5bb2eba7c6
Cristian Rojas Acuña <cristian@debian>
2017-11-03 17:07:41 -0300

    add update package cors

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const express = require ('express')
 const bodyParser = require('body-parser')
 const app = express()
-const api = require('./routes')
 const cors = require('cors')
+const api = require('./routes')
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
